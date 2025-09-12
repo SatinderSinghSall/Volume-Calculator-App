bafeb9b733fdbc49bf5e414091303f5f103d6f9c
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-09-12 21:19:31 +0530

    Added the GetView() Method & Layout Inflater in 'MyCustomAdapter.java' file.

--- app/src/main/java/com/example/volumecalculator/MyCustomAdapter.java
+++ app/src/main/java/com/example/volumecalculator/MyCustomAdapter.java
@@ -1,11 +1,15 @@
 package com.example.volumecalculator;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
@@ -22,5 +26,27 @@ public class MyCustomAdapter extends ArrayAdapter<Shape> {
     private static class MyViewHolder {
         TextView shapeName;
         ImageView shapeImage;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Shape shape = getItem(position);
+        MyViewHolder myViewHolder;
+        if (convertView == null) {
+            myViewHolder = new MyViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.grid_item_layout, parent, false);
+            myViewHolder.shapeName = (TextView) convertView.findViewById(R.id.textView);
+            myViewHolder.shapeImage = (ImageView) convertView.findViewById(R.id.imageView);
+            convertView.setTag(myViewHolder);
+        } else {
+            myViewHolder = (MyViewHolder) convertView.getTag();
+        }
+
+        myViewHolder.shapeName.setText(shape.getShapeText());
+        myViewHolder.shapeImage.setImageResource(shape.getShapeImage());
+
+        return convertView;
     }
 }
